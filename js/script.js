c4b4e7b37a44f431fd188abb1b81190eb073e738
Vasyl <example@gmail.com>
2021-03-20 15:43:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alert('Hello');
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer+5);
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+
+// const user = "Ivan";
+// alert(`Hello, ${user}`);
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(decr--);
+console.log(incr);
+
+console.log(5%2);
+
