d1bd2b58776c71cd4a27452cb14a27102f48fea9
Vasyl <example@gmail.com>
2021-03-21 15:08:05 +0200

    Цикли

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
 "use strict";
 
-// if (1) {
-//     console.log('Ok!');
-// } else {
-//     console.log('Error');
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// const num = 50;
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
-// } else {
-//     console.log('Ok!');
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num === 50) ? console.log('Ok!') : console.log('Error');  
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         break;
+//     }
+//     console.log(i);
+// }
 
-const num = 50;
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 51:
-        console.log('В точку!');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        continue;
+    }
+    console.log(i);
 }
