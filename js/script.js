4e71ccc22244c1bd299e29ad257c705087dd6d5b
Vasyl <example@gmail.com>
2021-03-21 14:21:13 +0200

    1 practice lesson

--- js/script.js
+++ js/script.js
@@ -1,28 +1,21 @@
 "use strict";
 
-// alert('Hello');
-// const result = confirm("Are you here?");
-// console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
 
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log(answer+5);
-// const answers = [];
 
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваше фамилия?', '');
-// answers[2] = prompt('Сколько Вам лет?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+};
 
-// document.write(answers);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// const user = "Ivan";
-// alert(`Hello, ${user}`);
-let incr = 10,
-    decr = 10;
-// incr++;
-// decr--;
-console.log(++incr);
-console.log(decr--);
-console.log(incr);
-
-console.log(5%2);
-
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
