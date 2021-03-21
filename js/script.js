15627cff7f839722941aec6e7379424260e5ed50
Vasyl <example@gmail.com>
2021-03-21 17:06:24 +0200

    Функции

--- js/script.js
+++ js/script.js
@@ -1,79 +1,49 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
+let num = 20;
 
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false 
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
 };
 
-// За допомогою циклу for
-
-// for (let i = 0; i <2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//           b = prompt('На сколько оцените его?', '');
-
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-    
-// }
-
-
-// За допомогою циклу while
-
-// let i = 0;
-// while (i <2) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//           b = prompt('На сколько оцените его?', '');
-//     i++;
-
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-// }
-
-
-// За допомогою циклу do
-
-let i = 0;
-do {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    i++;  
-      
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-} 
-    while (i < 2);
+showFirstMessage("Hello World!");
+console.log(num);
 
 
 
- if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
-     console.log('Просмотрено довольно мало фильмов');
- } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
-     console.log('Вы классический зритель');
- } else if (personalMovieDB.count >= 30) {
-     console.log('Вы киноман');
- } else {
-     console.log('Произошла ошибка');
- }
+function calc(a, b) {
+    return (a + b);
+}
 
- console.log(personalMovieDB);
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+ const anotherNum = ret();
+ console.log(anotherNum);
+
+
+
+// Function Expression
+ const logger = function() {
+    console.log("Hello!")
+ };
+
+ logger();
+
+
+
+
+//  Стрелочная функция
+
+const calcul = (a, b) => {
+    console.log('1');
+    return a + b;
+};
